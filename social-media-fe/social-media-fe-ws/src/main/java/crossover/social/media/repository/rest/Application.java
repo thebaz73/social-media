ef3c130cfd2f36a128128ea8a3622f9cc3063b2f
Marco Bazzoni <bazzoni.marco@gmail.com>
2015-07-10 00:31:16 +0200

    Refactoring

--- social-media-fe/social-media-fe-ws/src/main/java/crossover/social/media/repository/rest/Application.java
+++ social-media-fe/social-media-fe-ws/src/main/java/crossover/social/media/repository/rest/Application.java
@@ -1,12 +1,17 @@
-package crossover.social.media.repository;
+package crossover.social.media.repository.rest;
 
 import crossover.social.media.domain.Role;
 import crossover.social.media.domain.RoleName;
 import crossover.social.media.domain.User;
+import crossover.social.media.repository.RoleRepository;
+import crossover.social.media.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,12 +20,19 @@ import java.util.List;
  * Application
  * Created by bazzoni on 09/07/2015.
  */
+@Configuration
+@ComponentScan({"crossover.social.media.repository"})
 @EnableAutoConfiguration
 public class Application implements CommandLineRunner {
     @Autowired
     private RoleRepository roleRepository;
     @Autowired
     private UserRepository userRepository;
+
+    @Value("social.media.admin.usernname")
+    private String username = "admin";
+    @Value("social.media.admin.usernname")
+    private String password = "admin";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -41,9 +53,9 @@ public class Application implements CommandLineRunner {
                 roleRepository.save(Role);
             }
         }
-        final List<User> byUsername = userRepository.findByUsername("admin");
+        final List<User> byUsername = userRepository.findByUsername(username);
         if (byUsername.isEmpty()) {
-            User admin = new User("admin", "admin", Arrays.asList(roleRepository.findByRole(RoleName.ROLE_USER).get(0), roleRepository.findByRole(RoleName.ROLE_ADMIN).get(0)));
+            User admin = new User(username, password, Arrays.asList(roleRepository.findByRole(RoleName.ROLE_USER).get(0), roleRepository.findByRole(RoleName.ROLE_ADMIN).get(0)));
             userRepository.save(admin);
         }
     }
